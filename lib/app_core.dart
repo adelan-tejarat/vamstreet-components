da0baaa0342b7b7675ac42d2d113802c727be90d
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-04-14 09:44:41 +0330

    update: fix imports

--- lib/app_core.dart
+++ lib/app_core.dart
@@ -9,3 +9,5 @@ export 'package:vamstreet_components/src/core/enum/dynamic_color_enum.dart';
 export 'package:vamstreet_components/src/core/extensions/dynamic_color_extension.dart';
 export 'package:vamstreet_components/src/core/extensions/app_text_style_type.dart';
 export 'package:vamstreet_components/src/features/widgets/buttons/core/enums/button_type_enum.dart';
+export 'package:vamstreet_components/src/features/widgets/text_inputs/core/text_field_sizes.dart'
+    show TextFieldSizes;
