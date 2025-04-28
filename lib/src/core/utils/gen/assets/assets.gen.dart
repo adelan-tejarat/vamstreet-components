517c43bb02708e19663178f7d9d2c20ae0ba16e9
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-04-28 09:48:29 +0330

    feat(assets): remove old font and add `iran sans`

--- lib/src/core/utils/gen/assets/assets.gen.dart
+++ lib/src/core/utils/gen/assets/assets.gen.dart
@@ -10,44 +10,37 @@
 class $AssetsFontsGen {
   const $AssetsFontsGen();
 
-  /// File path: assets/fonts/Inter-Black.ttf
-  String get interBlack => 'assets/fonts/Inter-Black.ttf';
+  /// File path: assets/fonts/IRANSansMobile(FaNum).ttf
+  String get iRANSansMobileFaNum => 'assets/fonts/IRANSansMobile(FaNum).ttf';
 
-  /// File path: assets/fonts/Inter-Bold.ttf
-  String get interBold => 'assets/fonts/Inter-Bold.ttf';
+  /// File path: assets/fonts/IRANSansMobile(FaNum)_Black.ttf
+  String get iRANSansMobileFaNumBlack =>
+      'assets/fonts/IRANSansMobile(FaNum)_Black.ttf';
 
-  /// File path: assets/fonts/Inter-ExtraBold.ttf
-  String get interExtraBold => 'assets/fonts/Inter-ExtraBold.ttf';
+  /// File path: assets/fonts/IRANSansMobile(FaNum)_Bold.ttf
+  String get iRANSansMobileFaNumBold =>
+      'assets/fonts/IRANSansMobile(FaNum)_Bold.ttf';
 
-  /// File path: assets/fonts/Inter-ExtraLight.ttf
-  String get interExtraLight => 'assets/fonts/Inter-ExtraLight.ttf';
+  /// File path: assets/fonts/IRANSansMobile(FaNum)_Light.ttf
+  String get iRANSansMobileFaNumLight =>
+      'assets/fonts/IRANSansMobile(FaNum)_Light.ttf';
 
-  /// File path: assets/fonts/Inter-Light.ttf
-  String get interLight => 'assets/fonts/Inter-Light.ttf';
+  /// File path: assets/fonts/IRANSansMobile(FaNum)_Medium.ttf
+  String get iRANSansMobileFaNumMedium =>
+      'assets/fonts/IRANSansMobile(FaNum)_Medium.ttf';
 
-  /// File path: assets/fonts/Inter-Medium.ttf
-  String get interMedium => 'assets/fonts/Inter-Medium.ttf';
-
-  /// File path: assets/fonts/Inter-Regular.ttf
-  String get interRegular => 'assets/fonts/Inter-Regular.ttf';
-
-  /// File path: assets/fonts/Inter-SemiBold.ttf
-  String get interSemiBold => 'assets/fonts/Inter-SemiBold.ttf';
-
-  /// File path: assets/fonts/Inter-Thin.ttf
-  String get interThin => 'assets/fonts/Inter-Thin.ttf';
+  /// File path: assets/fonts/IRANSansMobile(FaNum)_UltraLight.ttf
+  String get iRANSansMobileFaNumUltraLight =>
+      'assets/fonts/IRANSansMobile(FaNum)_UltraLight.ttf';
 
   /// List of all assets
   List<String> get values => [
-    interBlack,
-    interBold,
-    interExtraBold,
-    interExtraLight,
-    interLight,
-    interMedium,
-    interRegular,
-    interSemiBold,
-    interThin,
+    iRANSansMobileFaNum,
+    iRANSansMobileFaNumBlack,
+    iRANSansMobileFaNumBold,
+    iRANSansMobileFaNumLight,
+    iRANSansMobileFaNumMedium,
+    iRANSansMobileFaNumUltraLight,
   ];
 }
 
